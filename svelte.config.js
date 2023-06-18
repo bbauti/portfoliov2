cb74bd11e8bbd52fded66bcd235fd89fbed4c30a
bbauti <bautistaigarzabal11@gmail.com>
2023-06-17 21:01:07 -0300

    Cloudflare pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,17 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
-	},
+        adapter: adapter({
+            // See below for an explanation of these options
+            routes: {
+                include: ['/*'],
+                exclude: ['<all>']
+            }
+        })
+    },
 
 	preprocess: [
 		preprocess({
