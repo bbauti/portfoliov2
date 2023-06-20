a69c6f24c31019b7000adb4d867455a246c48825
bbauti <bautistaigarzabal11@gmail.com>
2023-06-19 23:14:39 -0300

    fix about style y transicion en routing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,19 +6,32 @@
 	import { blur } from 'svelte/transition';
 	import { page } from '$app/stores';
  	export let data
+	import { browser } from '$app/environment'; 
+	let mobile = false
+	if (browser) {
+		if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+			mobile = true
+		} else {
+			mobile = false
+		};
+    }
 	let current = '/'
-	 const handleClick = (param) => () => {
+	const handleClick = (param) => () => {
 		current = param
 	} 
 </script>
 {#key data.pathname}
-	<div in:blur="{{ duration: 150, delay: 300 }}" out:blur="{{ duration: 150 }}">
-    	<slot />
-  	</div>
+	{#if mobile}
+		<slot />
+	{:else}
+		<div in:blur="{{ duration: 150, delay: 300 }}" out:blur="{{ duration: 150 }}">
+			<slot />
+		</div>
+	{/if}
   	<nav>
-		<a href='/' on:click={handleClick('/')} class="logo {current === '/' ? 'current' : ''}"><Icon icon="ant-design:home-filled" /></a>
-		<a href="/#contact" on:click={handleClick('/#contact')} class='links {current === '/#contact' ? 'current' : ''}'><Icon icon="uil:message" /></a>
-		<a href="/about" on:click={handleClick('/about')} class='links {current === '/about' ? 'current' : ''}'><Icon icon="fluent:person-48-filled" /></a>
-		<a href="/" id='disabled' class='links'><Icon icon="mdi:resume" /></a>
+		<a href='/' title="Home" on:click={handleClick('/')} class="logo {current === '/' ? 'current' : ''}"><Icon icon="ant-design:home-filled" /></a>
+		<a href="/#contact" title="Contacto" on:click={handleClick('/#contact')} class='links {current === '/#contact' ? 'current' : ''}'><Icon icon="uil:message" /></a>
+		<a href="/about" title="About" on:click={handleClick('/about')} class='links {current === '/about' ? 'current' : ''}'><Icon icon="fluent:person-48-filled" /></a>
+		<a href="/" title="CV" id='disabled' class='links'><Icon icon="mdi:resume" /></a>
 	</nav>
 {/key}
