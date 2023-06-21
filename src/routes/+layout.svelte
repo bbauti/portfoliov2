f1eeeeda8e78b55426f3791270622d3117dce389
bbauti <bautistaigarzabal11@gmail.com>
2023-06-21 20:33:18 -0300

    iniciar i18n

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,37 +1,56 @@
 <script>
-// @ts-nocheck
+	// @ts-nocheck
 
 	import '../app.scss';
 	import Icon from '@iconify/svelte';
 	import { blur } from 'svelte/transition';
 	import { page } from '$app/stores';
- 	export let data
-	import { browser } from '$app/environment'; 
-	let mobile = false
+	export let data;
+	import { browser } from '$app/environment';
+	let mobile = false;
 	if (browser) {
-		if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-			mobile = true
+		if (
+			/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+		) {
+			mobile = true;
 		} else {
-			mobile = false
-		};
-    }
-	let current = '/'
+			mobile = false;
+		}
+	}
+	let current = '/';
 	const handleClick = (param) => () => {
-		current = param
-	} 
+		current = param;
+	};
 </script>
+
 {#key data.pathname}
 	{#if mobile}
 		<slot />
 	{:else}
-		<div in:blur="{{ duration: 150, delay: 300 }}" out:blur="{{ duration: 150 }}">
+		<div in:blur={{ duration: 150, delay: 300 }} out:blur={{ duration: 150 }}>
 			<slot />
 		</div>
 	{/if}
-  	<nav>
-		<a href='/' title="Home" on:click={handleClick('/')} class="logo {current === '/' ? 'current' : ''}"><Icon icon="ant-design:home-filled" /></a>
-		<a href="/#contact" title="Contacto" on:click={handleClick('/#contact')} class='links {current === '/#contact' ? 'current' : ''}'><Icon icon="uil:message" /></a>
-		<a href="/about" title="About" on:click={handleClick('/about')} class='links {current === '/about' ? 'current' : ''}'><Icon icon="fluent:person-48-filled" /></a>
-		<a href="/" title="CV" id='disabled' class='links'><Icon icon="mdi:resume" /></a>
+	<nav>
+		<a
+			href="/"
+			title="Home"
+			on:click={handleClick('/')}
+			class="logo {current === '/' ? 'current' : ''}"><Icon icon="ant-design:home-filled" /></a
+		>
+		<a
+			href="/#contact"
+			title="Contacto"
+			on:click={handleClick('/#contact')}
+			class="links {current === '/#contact' ? 'current' : ''}"><Icon icon="uil:message" /></a
+		>
+		<a
+			href="/about"
+			title="About"
+			on:click={handleClick('/about')}
+			class="links {current === '/about' ? 'current' : ''}"
+			><Icon icon="fluent:person-48-filled" /></a
+		>
+		<a href="/" title="CV" id="disabled" class="links"><Icon icon="mdi:resume" /></a>
 	</nav>
 {/key}
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,23 +1,29 @@
 <main>
-    <h1 class='title'>Bautista Igarzabal</h1>
-    <h3 class='title'>Sobre mi</h3>
-    <p class='about'>
-        <span class='aboutsection'>
-            ¡Hola! Soy un <strong>desarrollador fullstack</strong> de <strong>Buenos Aires, Argentina</strong>.
-            Poseo experiencia en el desarrollo de aplicaciones tanto para escritorio como para dispositivos móviles.
-        </span>
-        <span class='aboutsection'>
-            Trabajé en diversos proyectos, destacando especialmente mi participación en <strong><a href="https://bitbucket.org/arrudeboy/inventory/src" target="_blank">Inventory App</a></strong>, un gran proyecto 
-            realizado durante mis estudios con mis compañeros y profesor, donde cumplí las funciones
-            de <strong>Developer y DevOps</strong>.
-        </span>
-        <span class='aboutsection'>
-            Siempre me ha gustado la programación, contando con sólidos conocimientos en <strong>HTML, CSS, JS, Python</strong>, y varias frameworks y librerías. 
-            Actualmente estoy aprendiendo PHP, intentando expandir aún más mis conocimientos.
-        </span>  
-        <span class='aboutsection'>
-            Además, tengo experiencia en el uso de <strong>metodologías ágiles</strong> como <strong>SCRUM</strong>. Soy una persona que se adapta 
-            rápidamente a nuevas tecnologías o metodologías, siempre enfocado en ofrecer un trabajo de <strong>alta calidad</strong>.
-        </span>
-    </p>
+	<h1 class="title">Bautista Igarzabal</h1>
+	<h3 class="title">Sobre mi</h3>
+	<p class="about">
+		<span class="aboutsection">
+			¡Hola! Soy un <strong>desarrollador fullstack</strong> de
+			<strong>Buenos Aires, Argentina</strong>. Poseo experiencia en el desarrollo de aplicaciones
+			tanto para escritorio como para dispositivos móviles.
+		</span>
+		<span class="aboutsection">
+			Trabajé en diversos proyectos, destacando especialmente mi participación en <strong
+				><a href="https://bitbucket.org/arrudeboy/inventory/src" target="_blank">Inventory App</a
+				></strong
+			>, un gran proyecto realizado durante mis estudios con mis compañeros y profesor, donde cumplí
+			las funciones de <strong>Developer y DevOps</strong>.
+		</span>
+		<span class="aboutsection">
+			Siempre me ha gustado la programación, contando con sólidos conocimientos en <strong
+				>HTML, CSS, JS, Python</strong
+			>, y varias frameworks y librerías. Actualmente estoy aprendiendo PHP, intentando expandir aún
+			más mis conocimientos.
+		</span>
+		<span class="aboutsection">
+			Además, tengo experiencia en el uso de <strong>metodologías ágiles</strong> como
+			<strong>SCRUM</strong>. Soy una persona que se adapta rápidamente a nuevas tecnologías o
+			metodologías, siempre enfocado en ofrecer un trabajo de <strong>alta calidad</strong>.
+		</span>
+	</p>
 </main>
